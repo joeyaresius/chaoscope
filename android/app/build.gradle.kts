2f7d82fa4e3e986aea63c46bc038736b7e0d074e
lucasbalancin <78731053+lucasbalancin@users.noreply.github.com>
2026-07-08 08:59:19 -0300

    Version bump

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.chaoscope"
         minSdk        = 26
         targetSdk     = 36
-        versionCode   = 15
-        versionName   = "0.3.0"
+        versionCode   = 16
+        versionName   = "0.3.1"
 
         externalNativeBuild {
             cmake {
